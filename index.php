97b092a73cdb5038efb86f751bb06e5097ff703a
Roberto B <roberto.butti@gmail.com>
2012-02-24 23:54:14 +0100

    gestione views con twig

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
 require_once("./vendor/.composer/autoload.php");
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path'       => __DIR__.'/views',
+    'twig.class_path' => __DIR__.'/vendor/twig/lib',
+));
+
 $app->get('/page/{slug}', function ($slug) use ($app) {
-    return 'Load PAGES: '.$app->escape($slug);
+  $template_name='pages/'.$app->escape($slug).'.twig';
+  if (file_exists(__DIR__.'/views/'.$template_name)) {
+    return $app['twig']->render($template_name, array(
+      'slug' => $slug,
+    ));
+  } else {
+    $message = "Template ".$app->escape($slug)." not exists";
+    return new Symfony\Component\HttpFoundation\Response($message, 404);
+  }
 });
 
 $app->get('/', function () {
     return 'Home ';
 });
+$app['debug'] = true;
 
 $app->run();
